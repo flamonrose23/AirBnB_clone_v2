0d5bbadaf059976398260de55b8cf7e9b9aaf166
flamonrose23 <fatinemar20@icloud.com>
2024-05-03 19:08:38 -0700

    upload

---
+++ 100-clean_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from fabric.api import *
+import os
+
+env.hosts = ['34.234.204.95', '54.89.135.154']
+
+
+def do_clean(number=0):
+    """
+    Deleting out-of-date archives
+    Arguments:
+        number (int): The number of archives to keep.
+
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
