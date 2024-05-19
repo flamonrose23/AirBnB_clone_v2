70d360df93e45f45466b25af7408359dbbb624ee
flamonrose23 <fatinemar20@icloud.com>
2024-05-19 08:57:44 -0700

    Testing task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Writing script starting Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Returning Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returning HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """
+    Displaying “C ” followed by value of text variable
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """
+    Displaying “Python ”, followed by value of text variable
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
