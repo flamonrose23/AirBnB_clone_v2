83b6c76f3a83d456448db9a8a9c95099a741722a
flamonrose23 <fatinemar20@icloud.com>
2024-05-19 09:03:22 -0700

    Testing task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Writing script starting Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Returning Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returning HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """
+    Displaying “C ” followed by value of text variable
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """
+    Displaying “Python ”, followed by value of text variable
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """
+    Displaying “n is a number” only if n is an integer
+    """
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
